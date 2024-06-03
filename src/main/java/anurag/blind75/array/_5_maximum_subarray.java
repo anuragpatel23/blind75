1c1d8fb95f76bd29b37c36441e23e87df0647dde
Anurag Patel <mail2anuragpatel@gmail.com>
2024-06-03 16:58:44 +0530

    maximum sub array

---
+++ src/main/java/anurag/blind75/array/_5_maximum_subarray.java
@@ -0,0 +1,37 @@
+package anurag.blind75.array;
+
+public class _5_maximum_subarray {
+    public static void main(String... args){
+        int[]   nums={-2,1,-3,4,-1,2,1,-5,4};
+        int result1 = maximumSubArrayBrutForce(nums);
+        int result2 = maximumSubArray(nums);
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+    public static int maximumSubArrayBrutForce(int[] nums){
+        int maxSubArraySum=0;
+        for(int i=0;i<nums.length;i++){
+            int localSum=0;
+            for(int j=i;j<nums.length;j++){
+                localSum=localSum+nums[j];
+                if(localSum>maxSubArraySum)
+                    maxSubArraySum=localSum;
+            }
+        }
+        return maxSubArraySum;
+    }
+
+    public static int maximumSubArray(int[] nums){
+        int currentSum=0;
+        int maxSum=nums[0];
+        for(int i=0;i<nums.length; i++){
+            if(currentSum<0)
+                currentSum=0;
+            currentSum=currentSum+nums[i];
+            maxSum=Math.max(currentSum,maxSum);
+        }
+        return maxSum;
+    }
+}

8633913e3a94ee7e799c21adc90acbe91a916334
Anurag Patel <mail2anuragpatel@gmail.com>
2024-06-15 18:42:36 +0530

    modification in maximum product arrray to address edge case [0,10,10,10,10,10,10,10,10,10,-10,10,10,10,10,10,10,10,10,10,0]

--- src/main/java/anurag/blind75/array/_6_maximum_product_subarray.java
+++ src/main/java/anurag/blind75/array/_6_maximum_product_subarray.java
@@ -8,8 +8,37 @@ public class _6_maximum_product_subarray {
         //using Dynamic programming
         int result2=maxProductSubArrayDP(nums);
 
+        //it does not pass test case [0,10,10,10,10,10,10,10,10,10,-10,10,10,10,10,10,10,10,10,10,0]
+        //for this we need to use double value
+        int[] nums1={0,10,10,10,10,10,10,10,10,10,-10,10,10,10,10,10,10,10,10,10,0};
+        int result3=maxProductSubArrayDouble(nums1);
+
         System.out.println(result1);
         System.out.println(result2);
+        System.out.println(result3);
+    }
+
+    private static int maxProductSubArrayDouble(int[] nums) {
+        int n = nums.length;
+        double prefix=1,suffix=1;
+        double ans=nums[0];
+
+        for(int i=0;i<n;i++){
+
+            //if any of l or r become 0 then update it to 1
+            prefix = prefix==0 ? 1 : prefix;
+            suffix = suffix==0 ? 1 : suffix;
+
+            prefix *= (double) nums[i];   //prefix product
+            suffix *= (double) nums[n-1-i];    //suffix product
+
+            // System.out.println(l+" "+r);
+
+            ans = Math.max(ans,Math.max(prefix,suffix));
+
+        }
+
+        return (int) ans;
     }
 
     public static int maxProductSubArrayBrutForce(int[] nums){

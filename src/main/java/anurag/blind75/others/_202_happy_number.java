70bc88463d979056810d7e7d8aa9f9be9e214965
Anurag Patel <mail2anuragpatel@gmail.com>
2024-07-08 22:39:27 +0530

    happy number

---
+++ src/main/java/anurag/blind75/others/_202_happy_number.java
@@ -0,0 +1,35 @@
+package anurag.blind75.others;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+public class _202_happy_number {
+
+    public static void main(String... args){
+        boolean res=isHappy(19);
+        System.out.println("19 is a Happy number: "+res);
+    }
+
+    private static boolean isHappy(int n) {
+    HashSet<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+        seen.add(n);
+        n = getNextHappyNumber(n);
+    }
+
+        return n == 1;
+
+}
+
+    private static int getNextHappyNumber(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}

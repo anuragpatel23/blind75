90b7b3485312734eca0a113017b531d5ec98ad5f
Anurag Patel <33739956+anuragpatel23@users.noreply.github.com>
2024-07-09 22:42:57 +0530

    Update _169_majority_element.java

--- src/main/java/anurag/blind75/others/_169_majority_element.java
+++ src/main/java/anurag/blind75/others/_169_majority_element.java
@@ -6,10 +6,13 @@ public class _169_majority_element {
         int[] nums={3,2,3};
         int result = majorityElement(nums);
         System.out.println("majority element in array is : "+result);
+        
     }
     private static int majorityElement(int[] nums) {
+        
         int count=1;
         int curr=nums[0];
+        
         for(int i=1; i<nums.length; i++){
 
             if(count==0){

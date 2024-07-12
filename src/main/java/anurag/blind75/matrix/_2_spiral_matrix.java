9e28a8ca473e05790340101819021d5d4754d2e2
Anurag Patel <mail2anuragpatel@gmail.com>
2024-07-12 20:11:04 +0530

    spiral matrix solution 3

--- src/main/java/anurag/blind75/matrix/_2_spiral_matrix.java
+++ src/main/java/anurag/blind75/matrix/_2_spiral_matrix.java
@@ -6,15 +6,70 @@ public class _2_spiral_matrix {
 
     public static void main(String... args){
         int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-//        List<Integer> result1 = spiralOrderShrinkingSolution(matrix);
+        List<Integer> result1 = spiralOrderShrinkingSolution(matrix);
         List<Integer> result2 = spiralOrderVisitedHashSetSolution(matrix);
         List<Integer> result3 = spiralOrderVisitedSolution(matrix);
 
-//        System.out.println("Spiral matrix - "+ result1.toString());
+        System.out.println("Spiral matrix - "+ result1.toString());
         System.out.println("Spiral matrix - "+ result2.toString());
         System.out.println("Spiral matrix - "+ result3.toString());
     }
 
+    /**
+     *
+     * @param matrix
+     * @return
+     * @apiNote this solution uses the simple methodology of shrinking the boundaries
+     */
+    private static List<Integer> spiralOrderShrinkingSolution(int[][] matrix){
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right along the top row
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            // Traverse from top to bottom along the right column
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                // Traverse from right to left along the bottom row
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                // Traverse from bottom to top along the left column
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     *
+     * @param matrix
+     * @return
+     * @apiNote This solution uses the methodology of storing the visited node in a HashSet
+     */
     private static List<Integer> spiralOrderVisitedHashSetSolution(int[][] matrix){
 
         List<Integer> result = new ArrayList<>();
@@ -51,6 +106,13 @@ public class _2_spiral_matrix {
         return result;
     }
 
+    /**
+     *
+     * @param matrix
+     * @return
+     * @apiNote thi solution uses the methodology same as previous solution but reduces the space complexity
+     * as we are not using Hashset instead we assign som random value to already visited node
+     */
     private static List<Integer> spiralOrderVisitedSolution(int[][] matrix){
         int rows = matrix.length;
         int columns = matrix[0].length;

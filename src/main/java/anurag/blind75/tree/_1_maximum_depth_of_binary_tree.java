20efe81b08c46e465cfb53942e652883163f4fcd
Anurag Patel <mail2anuragpatel@gmail.com>
2024-07-13 21:11:57 +0530

    maximum depth search

---
+++ src/main/java/anurag/blind75/tree/_1_maximum_depth_of_binary_tree.java
@@ -0,0 +1,23 @@
+//package anurag.blind75.tree;
+//
+//public class _1_maximum_depth_of_binary_tree {
+//    void main(){
+//        TreeNode tree = new TreeNode();
+//        tree.left=new TreeNode(3, new TreeNode(9,null, null),);
+//
+//        int[] tree=maxDepth();
+//    }
+//}
+//
+//class TreeNode {
+//      int val;
+//      TreeNode left;
+//      TreeNode right;
+//      TreeNode() {}
+//      TreeNode(int val) { this.val = val; }
+//      TreeNode(int val, TreeNode left, TreeNode right) {
+//          this.val = val;
+//          this.left = left;
+//          this.right = right;
+//      }
+// }

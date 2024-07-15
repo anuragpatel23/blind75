6765117112578b9b75009b30113cef7afb65f8ba
Anurag Patel <mail2anuragpatel@gmail.com>
2024-07-15 16:41:38 +0530

    maximum depth of binary tree

--- src/main/java/anurag/blind75/tree/_1_maximum_depth_of_binary_tree.java
+++ src/main/java/anurag/blind75/tree/_1_maximum_depth_of_binary_tree.java
@@ -30,6 +30,7 @@ class TreeNode {
       TreeNode() {}
       TreeNode(int val) { this.val = val; }
       TreeNode(int val, TreeNode left, TreeNode right) {
+
           this.val = val;
           this.left = left;
           this.right = right;

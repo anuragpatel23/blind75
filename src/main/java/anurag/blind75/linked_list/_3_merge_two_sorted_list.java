3c5a9e95ed8c87ffdd1d386ca60297c8cbd8174f
Anurag Patel <mail2anuragpatel@gmail.com>
2024-07-17 17:54:11 +0530

    updatee

--- src/main/java/anurag/blind75/linked_list/_3_merge_two_sorted_list.java
+++ src/main/java/anurag/blind75/linked_list/_3_merge_two_sorted_list.java
@@ -5,7 +5,6 @@ public class _3_merge_two_sorted_list {
     public static void main(String... args){
         ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4,null)));
         ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4,null)));
-
         ListNode result = mergeTwoLists(list1, list2);
         System.out.println("sorted : "+result);
     }

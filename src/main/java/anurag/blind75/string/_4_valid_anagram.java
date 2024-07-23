630fd4b123f0237e9cfbb5992aa7fc1b1dec37e7
Anurag Patel <mail2anuragpatel@gmail.com>
2024-07-23 20:57:14 +0530

    valid anagram

--- src/main/java/anurag/blind75/string/_4_valid_anagram.java
+++ src/main/java/anurag/blind75/string/_4_valid_anagram.java
@@ -10,10 +10,29 @@ public class _4_valid_anagram {
 
         boolean result1 = isAnagramBruteForce(s, t);
         boolean result2 = isAnagramSorting(s, t);
+        boolean result3 = isAnagramBest(s, t);
         System.out.println(result1);
         System.out.println(result2);
+        System.out.println(result3);
     }
 
+    private static boolean isAnagramBest(String s, String t){
+        if(s.length() != t.length())
+            return false;
+
+        int[] count= new int[26];
+        for (int i=0; i<s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int j=0; j<26; j++){
+            if (count[j] !=0){
+                return false;
+            }
+        }
+        return true;
+    }
 
     private static boolean isAnagramSorting(String s, String t){
         String str1 = Arrays.stream(s.split("")).sorted().collect(Collectors.joining(""));
